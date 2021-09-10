93182e0e5b6f8e8c90672cb8d0b005aededab0d8
BGabesz <bogdan.gabesz@gmail.com>
2021-09-10 16:01:32 +0200

    first commit

---
+++ KincsesLada1/src/kincseslada1/KincsesLada1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kincseslada1;
+
+/**
+ *
+ * @author hallgato
+ */
+public class KincsesLada1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
